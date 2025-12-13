fdd1cd363eb479bc1573234df9e86b797439d1bd
BioInsight Lab <dev@bioinsightlab.com>
2025-12-13 20:23:13 +0900

    docs: PRD 湲곕컲 ?꾩껜 ?먭? 由ы룷???묒꽦 諛?jest.config.js 以묐났 肄붾뱶 ?섏젙

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -30,66 +30,6 @@ module.exports = createJestConfig(customJestConfig);
 
 
 
-const createJestConfig = nextJest({
-  // Next.js 앱의 경로를 제공하여 next.config.js와 .env 파일을 로드
-  dir: "./",
-});
-
-// Jest에 전달할 커스텀 설정 추가
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[jt]s?(x)",
-  ],
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/**/*.stories.{js,jsx,ts,tsx}",
-    "!src/**/__tests__/**",
-  ],
-};
-
-// createJestConfig는 비동기이므로 next/jest가 Next.js 설정을 로드할 수 있게 함
-module.exports = createJestConfig(customJestConfig);
-
-
-
-
-const createJestConfig = nextJest({
-  // Next.js 앱의 경로를 제공하여 next.config.js와 .env 파일을 로드
-  dir: "./",
-});
-
-// Jest에 전달할 커스텀 설정 추가
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[jt]s?(x)",
-  ],
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/**/*.stories.{js,jsx,ts,tsx}",
-    "!src/**/__tests__/**",
-  ],
-};
-
-// createJestConfig는 비동기이므로 next/jest가 Next.js 설정을 로드할 수 있게 함
-module.exports = createJestConfig(customJestConfig);
-
-
-
-
 
 
 
